918b7e69390539134a81be7b27bcb79341dad696
Mukhammad <m.rustamkhonov@student.inha.uz>
2019-12-14 13:19:49 +0500

    Biba

---
+++ src/sample/Persons/Administrator.java
@@ -0,0 +1,7 @@
+package sample.Persons;
+
+public class Administrator {
+    private final String name = "INHA";
+    private final String id = "admin01";
+    private final String password = "admin";
+}
