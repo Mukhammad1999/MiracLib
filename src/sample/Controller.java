74b238865e4293031d31191819456aa9b0861f05
Mukhammad <m.rustamkhonov@student.inha.uz>
2019-12-14 21:47:00 +0500

    Automatic SignUp SignIn pages after pressing button

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -22,13 +23,14 @@ public class Controller implements Initializable{
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("SignUpPage.fxml"));
             Parent root1 = (Parent) fxmlLoader.load();
-            Stage stage = new Stage();
+
+
+            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root1));
             stage.show();
 
         }
         catch (Exception e){
-
         }
 
     }
@@ -36,14 +38,18 @@ public class Controller implements Initializable{
     public  void SignInPageC(ActionEvent event){
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("SignIn Page.fxml"));
+
             Parent root1 = (Parent) fxmlLoader.load();
-            Stage stage = new Stage();
+
+
+            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root1));
             stage.show();
 
         }
-        catch (Exception e){
 
+
+        catch (Exception e){
         }
 
     }
