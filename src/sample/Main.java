70e1a5efb72b686d2f73d992c6e65f966d204a94
Mukhammad <m.rustamkhonov@student.inha.uz>
2019-12-15 14:42:24 +0500

    not

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.show();
+
     }
 
 
