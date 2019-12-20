3ab8a5be5167ecfb52c5ebebbec5ce58cca94ccf
khursandov <xursandov_s@mail.ru>
2019-12-21 02:17:38 +0500

    Sign up done with radiobox

--- src/sample/SignUpController.java
+++ src/sample/SignUpController.java
@@ -9,15 +9,16 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.sql.*;
 
 public class SignUpController<idd> {
     @FXML
-    private RadioButton StudentRadioButton;
+    private RadioButton studentRadioButton;
     @FXML
-    private RadioButton LibRaioButton;
+    private RadioButton librarianRadioButton;
     @FXML
     private TextField loginarea;
     @FXML
@@ -27,30 +28,44 @@ public class SignUpController<idd> {
     @FXML
     private Label forlogin;
     @FXML
+    private Label forRadioButton;
+    @FXML
     private Label forpass;
     @FXML
     private String is_admin;
     private Label incons;
     Connection conn;
-    String idd;
+
     public void SignUpBut(ActionEvent event) throws IOException, SQLException {
         String login = loginarea.getText();//this variable to be passed in tables
         String password = passwordarea.getText();//this variable to be passed in tables
-
         if(loginarea.getText().isEmpty()) {
             forlogin.setText("The Login area cannot be empty");
         }
         if(passwordarea.getText().isEmpty()){
             forpass.setText("Password area cannot be empty");
         }
-        if ((!loginarea.getText().isEmpty()) && (!passwordarea.getText().isEmpty()) /*&& (!e_mailarea.getText().isEmpty())*/) {
+        if (studentRadioButton.selectedProperty().getValue() || librarianRadioButton.selectedProperty().getValue()) {
+            forRadioButton.setText("You must choose one of this!");
+        }
+        if ( (!loginarea.getText().isEmpty()) && (!passwordarea.getText().isEmpty()) && (studentRadioButton.selectedProperty().getValue() || librarianRadioButton.selectedProperty().getValue()) ) {
             //logic for adding person to be here
             //A and L firstletters should be for Librarian and Administrator
             //S for students
             try {
+                System.out.println(studentRadioButton.selectedProperty().getValue());
                 conn = DriverManager.getConnection("jdbc:postgresql://localhost/Micralib", "postgres", "rahimho1499");
                 Statement statement = conn.createStatement();
-                statement.execute("INSERT INTO users (id, password, is_admin, is_active) VALUES ('"+loginarea.getText()+"','"+passwordarea.getText()+"' ,true,true)");
+//                Add student
+                if ( studentRadioButton.selectedProperty().getValue() ) {
+                    statement.execute("INSERT INTO users (id, password, is_admin, is_active, is_student, is_librarian) " +
+                            "VALUES ('"+loginarea.getText()+"', '"+passwordarea.getText()+"', false, false, true, false)");
+                }
+//                Add librarian
+                if (librarianRadioButton.selectedProperty().getValue()){
+                    statement.execute("INSERT INTO users (id, password, is_admin, is_active, is_student, is_librarian) " +
+                            "VALUES ('"+loginarea.getText()+"', '"+passwordarea.getText()+"', false, false, false, true)");
+                }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -59,15 +74,6 @@ public class SignUpController<idd> {
         }
     }
     //Add you logic here
-    public boolean SignUpRadioButtonSelected(){
-        if(StudentRadioButton.getText() == "librarian"){
-            return false;
-        }
-        else{
-
-            return true;
-        }
-    }
 
 
 
